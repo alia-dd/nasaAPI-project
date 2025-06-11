02c36de4f035acefab1214f190034375f0e9a5f9
alia-dd <meyran99@gmail.com>
2025-06-11 17:59:04 +0300

    again:((

--- projectnasa/src/App.js
+++ projectnasa/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Route,Routes, BrowserRouter,Link} from 'react-router-dom';
+import {Route,Routes, HashRouter as Router,Link} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Astronomy_video from './Astronomy_video';
@@ -10,16 +10,18 @@ import Footer from './Footer';
 import './index.css';
 function App() {
   return(
-  <div>
-    {<Navbar/>}
-    <Routes>
-      <Route element={<Home/>} path="/" exact />
-      <Route element={<Astronomy_video/>} path="/astronomy_video"/>
-      <Route element={<Astronomy_img/>} path="/astronomy_img" />
-      <Route element={<About/>} path="/about"/>
-    </Routes>
-    {<Footer/>}
-  </div>
+    <Router>
+      <div>
+        {<Navbar/>}
+        <Routes>
+          <Route element={<Home/>} path="/" exact />
+          <Route element={<Astronomy_video/>} path="/astronomy_video"/>
+          <Route element={<Astronomy_img/>} path="/astronomy_img" />
+          <Route element={<About/>} path="/about"/>
+        </Routes>
+        {<Footer/>}
+      </div>
+  </Router>
   );
 }
 
